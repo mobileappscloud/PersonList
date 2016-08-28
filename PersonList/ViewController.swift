0749c035e9f0c61d3d4dd258f8e73380471732da
mobileappscloud <doc92606@gmail.com>
2016-08-28 10:41:02 -0700

    Initial Commit

---
+++ PersonList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonList
+//
+//  Created by Faisal Syed on 8/28/16.
+//  Copyright © 2016 Faisal Syed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
